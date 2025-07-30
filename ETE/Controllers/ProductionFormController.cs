06c2bb45b7f285fc2ebf2a3a56aca4e6087c14f6
Jose Lugo <jose.lugo@mesa.com>
2025-07-30 09:23:58 -0600

    Metodos para obtener codigos, obtener codigo por descripcion y metodo para registrar

--- ETE/Controllers/ProductionFormController.cs
+++ ETE/Controllers/ProductionFormController.cs
@@ -1,4 +1,5 @@
-﻿using ETE.Models;
+﻿using ETE.Dtos;
+using ETE.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -35,6 +36,15 @@ namespace ETE.Controllers
         }
 
         [HttpGet]
+        [Route("GetCodes")]
+        public async Task<IActionResult> GetCodes()
+        {
+            var codes = await _context.Codes.AsNoTracking().ToListAsync();
+
+            return Ok(codes);
+        }
+
+        [HttpGet]
         [Route("GetProcessesByLine/{lineId}")]
         public async Task<IActionResult> GetProcessesBy(int lineId)
         {
@@ -56,6 +66,95 @@ namespace ETE.Controllers
                             .ToListAsync();
 
             return Ok(machines);
+        }
+
+        [HttpGet]
+        [Route("GetReasonByCodes/{codesId}")]
+        public async Task<IActionResult> GetReasonByCodes(int codesId)
+        {
+            var reasons = await _context.Reason
+                                .Where(r => r.CodeId == codesId)
+                                .AsNoTracking()
+                                .ToListAsync();
+
+            return Ok(reasons);
+        }
+
+        [HttpPost]
+        [Route("RegisterProduction")]
+        public async Task<IActionResult> RegisterProduction([FromBody] ProductionRegisterDto productionDto)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                if(productionDto.DeadTimes != null && productionDto.DeadTimes.Any())
+                {
+                    foreach(var dt in productionDto.DeadTimes)
+                    {
+                        var codeExists = await _context.Codes.AnyAsync(c => c.Id == dt.CodeId);
+                        var reasonExists = await _context.Reason.AnyAsync(r => r.Id == dt.ReasonId);
+
+                        if(!codeExists || !reasonExists)
+                        {
+                            return BadRequest("Código o razón de tiempo muerto no válido");
+                        }
+                    }
+                }
+
+                List<DeadTimes> createdDeadTimes = new List<DeadTimes>();
+                if(productionDto.DeadTimes != null && productionDto.DeadTimes.Any())
+                {
+                    foreach(var deadTimeDto in productionDto.DeadTimes)
+                    {
+                        var deadTime = new DeadTimes
+                        {
+                            Minutes = deadTimeDto.Minutes,
+                            CodeId = deadTimeDto.CodeId,
+                            ReasonId = deadTimeDto.ReasonId,
+                        };
+
+                        _context.DeadTimes.Add(deadTime);
+                        createdDeadTimes.Add(deadTime);
+                    }
+                    await _context.SaveChangesAsync();
+                }
+
+                var production = new Production
+                {
+                    PartNumber = productionDto.PartNumber,
+                    PieceQuantity = productionDto.PieceQuantity,
+                    HourId = productionDto.HourId,
+                    LinesId = productionDto.LinesId,
+                    ProcessId = productionDto.ProcessId,
+                    MachineId = productionDto.MachineId,
+                };
+
+                if (createdDeadTimes.Any())
+                {
+                    foreach(var dt in createdDeadTimes)
+                    {
+                        production.DeadTimesId = dt.Id;
+                    }
+                }
+
+                _context.Production.Add(production);
+                await _context.SaveChangesAsync();
+
+                return Ok(new
+                {
+                    Succes = true,
+                    ProductionId = production.Id,
+                    DeadTimesIds = createdDeadTimes.Select(dt => dt.Id).ToList()
+                });
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, $"Error interno del servidor: {ex.Message}");
+            }
         }
     }
 }
