18666e27ed831a7b568714325355df97fa430d65
Jose Lugo <jose.lugo@mesa.com>
2025-07-30 09:27:12 -0600

    DTOs nuevos

---
+++ ETE/Dtos/ProductionRegisterDto.cs
@@ -0,0 +1,28 @@
+﻿using ETE.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace ETE.Dtos
+{
+    public class ProductionRegisterDto
+    {
+        [Required]
+        public string PartNumber { get; set; }
+
+        [Required]
+        public int PieceQuantity { get; set; }
+
+        [Required]
+        public int HourId { get; set; }
+
+        [Required]
+        public int LinesId { get; set; } 
+
+        [Required]
+        public int ProcessId { get; set; }
+
+        [Required]
+        public int MachineId { get; set; }
+
+        public List<DeadTimeDto> DeadTimes { get; set; }
+    }
+}
